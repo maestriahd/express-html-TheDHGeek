481e39b6e4264fb6bfe22b750a0e83e998a662e2
Roger Martinez <roger.martinez@uniandes.edu.co>
2017-03-04 21:38:49 -0500

    modificación al CSS y creación de objetos inline

--- routes/emisora.js
+++ routes/emisora.js
@@ -5,10 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('emisora',
     {
-      title: 'Proyecto Séneca',
-      texto: 'Emisora de la comunidad uniandina - La hacemos todos',
-      logo: 'http://www.proyectoseneca.com/wp-content/uploads/2017/01/cropped-Logo-01.png'
-  
+
+      texto: 'La hacemos todos',
+      logo: 'http://www.proyectoseneca.com/wp-content/uploads/2017/01/cropped-Logo-01-e1488680044708.png'
+
   }
   );
 });
